6a940ffa9093bd8ba5338423134205f71156fd18
Monty Thibault <montythibault@gmail.com>
2017-09-07 17:21:01 -0700

    rangeslider in progress

--- src/rangeslider.js
+++ src/rangeslider.js
@@ -1,6 +1,7 @@
-function RangeSlider(axis) {
+function RangeSlider(principal_axis, secondary_axis) {
 
-	this.axis = axis;
+	this.axis = principal_axis;
+	this.saxis = secondary_axis;
 
 
 	// These will be delegated to the axis class.
@@ -10,7 +11,27 @@ function RangeSlider(axis) {
 
 
 RangeSlider.prototype.draw = function(context, toX, toY) {
+
+	if(this.axis.orientation) {
+		
+		var startX = 0,
+		    startY = toY(this.saxis.max) - 20,
+		    width = toX(this.axis.max),
+		    height = 20;
+
+	} else {
 	
+		var startX = toX(this.saxis.max) - 20,
+		    startY = 0,
+		    width = 20,
+		    height = toY(this.axis.max);
+
+	}
+
+	context.fillStyle = '#F5F5F5';
+	context.fillRect(startX, startY, width, height);
+
+
 	
 
 };
@@ -30,7 +51,9 @@ RangeSlider.prototype.onactive = function() {
 };
 
 RangeSlider.prototype.ondblclick = function() {
-
+	this.axis.min = this.axis.minLimit;
+	this.axis.max = this.axis.maxLimit;
 };
+
 
 module.exports = RangeSlider;
