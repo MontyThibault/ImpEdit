77ce6e01c68a032a532427c6acc01029cdc8ced2
Monty Thibault <montythibault@gmail.com>
2017-09-17 11:21:30 -0700

    Implemented toBuffer (untested)

--- src/lineeditor.js
+++ src/lineeditor.js
@@ -5,6 +5,8 @@ var Line = require('./line.js');
 function LineEditor(graph) {
 
 	this.line = new Line();
+
+	// This array stays sorted by x-coordinate.
 	this.controlpoints = [];
 
 	this.line.points = this.controlpoints;
@@ -15,10 +17,6 @@ function LineEditor(graph) {
 
 
 LineEditor.prototype.draw = function(context, toX, toY) {
-
-	this.controlpoints.sort(function(a, b) {
-		return a.x - b.x;
-	});
 
 	this.line.draw(context, toX, toY);
 
@@ -34,6 +32,8 @@ LineEditor.prototype.addControlPoint = function(x, y) {
 
 	this.controlpoints.push(cp);
 	this.graph.mousecontrol.addObject(cp);
+
+	this.sort();
 };
 
 LineEditor.prototype.removeControlPoint = function(o) {
@@ -45,10 +45,76 @@ LineEditor.prototype.removeControlPoint = function(o) {
 	if(i > -1) {
 		this.controlpoints.splice(i, 1);
 	}
+
+	this.sort();
 };
 
 
-LineEditor.prototype.toBuffer = function() {
+LineEditor.prototype.sort = function() {
+
+	this.controlpoints.sort(function(a, b) {
+		return a.x - b.x;
+	});
+
+};
+
+
+LineEditor.prototype.toBuffer = function(buffer, samplerate) {
+
+	// We assume this.graph.xAxis is calibrated to seconds.
+
+	if(this.controlpoints.length < 2) {
+		buffer.fill(0);
+		return;
+	}
+
+
+	// cp1 is the control point directly before the current point
+	// cp2 is the control point directly after the current point
+	var cp1 = this.controlpoints[0].x,
+		cp2 = this.controlpoints[1].x;
+
+	var cpi = 1;
+	var escape = false;
+
+	for(var i = 0; i < buffer.length; i++) {
+
+		var time = i / samplerate;
+
+
+		while(time > cp2) {
+
+			cpi++;
+
+			// If this point is beyond all control points.
+			if(this.cpi === this.controlpoints.length) {
+				buffer[i] = 0;
+
+				escape = true;
+				break;
+			}
+
+			cp1 = cp2;
+			cp2 = this.controlpoints[cpi].x;
+
+		}
+
+		if(escape) {
+			escape = false;
+			continue;
+		}
+
+ 		
+ 		// If this point is before all control points.
+		if(time < cp1) {
+			buffer[i] = 0;
+			continue;
+		}
+
+
+		buffer[i] = (time - cp1.x) * (cp2.y - cp1.y) / (cp2.x - cp1.x);
+
+	}
 
 };
 
