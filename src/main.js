1ec220effca6cf8f5091556401a18585aa9b0969
Monty Thibault <montythibault@gmail.com>
2022-09-12 09:32:40 -0700

    Delegated audioDOM.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 var Graph = require("./graph.js");
 var Audio = require("./audio.js");
+var attachAudioDOM = require("./audioDOM.js");
 
 
 var graph_canvas = document.getElementById('screen');
@@ -35,54 +36,4 @@ window.onresize();
 
 
 var audio = new Audio();
-
-
-
-var convolution_enabled = document.getElementById('convolution_enabled');
-var fft_enabled = document.getElementById('fft_enabled');
-var range_label = document.getElementById('range_label');
-var range_slider = document.getElementById('range_slider');
-
-
-convolution_enabled.onclick = function(e) {
-
-	if(convolution_enabled.checked === true) {
-		audio.convolve_enable();
-	} else {
-		audio.convolve_disable();
-	}
-
-};
-
-
-fft_enabled.onclick = function(e) {
-
-	if(fft_enabled.checked === true) {
-		audio.fft_enable();
-	} else {
-		audio.fft_disable();
-	}
-
-};
-
-
-var range_min = 20;
-var range_max = 20000;
-
-var range_log_base = 10;
-
-range_slider.min = Math.log(range_min) / Math.log(range_log_base);
-range_slider.max = Math.log(range_max) / Math.log(range_log_base);
-
-range_slider.oninput = function() {
-
-	var n = Math.floor(Math.pow(range_log_base, this.value));
-
-	range_label.innerHTML = n + ' Hz';
-	audio.lowpass_cutoff = n;
-
-};
-
-range_slider.value = (Math.log(range_min) + Math.log(range_max)) 
-		/ (2 * Math.log(range_log_base));
-range_slider.oninput();
+attachAudioDOM(audio);
