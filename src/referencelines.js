3f92f23167d8e643beb7d30661751f78c762ccde
Monty Thibault <montythibault@gmail.com>
2022-09-12 09:32:39 -0700

    Created a referencelines.js that controls z-index in refernce line drawing. Renamed referencelines.js -> referencelinesaxis.js

--- src/referencelines.js
+++ src/referencelines.js
@@ -1,191 +1,74 @@
+var ReferenceLinesAxis = require('./referencelinesaxis.js');
 
 
 function ReferenceLines(principal_axis, secondary_axis) {
-	this.axis = principal_axis;
 
-	// Required only for drawing
-	this.saxis = secondary_axis;
+	this.xRef = new ReferenceLinesAxis(principal_axis, secondary_axis);
+	this.yRef = new ReferenceLinesAxis(secondary_axis, principal_axis);
 
-	// How many small lines between large lines (recursive)
-	this.line_multiples = 10;
-
-	// Increase this to see less frequent 
-	this.minimum_label_distance = 100; //px
 }
 
-ReferenceLines.prototype._iterateIntervalOverAxis = function(interval, f) {
-
-	var begin = Math.ceil(this.axis.min / interval) * interval,
-		end = Math.floor(this.axis.max / interval) * interval;
-
-	for(var j = begin; j <= end; j += interval) {
-
-		f.call(this, j);
-
-	}
-
-};
-
-ReferenceLines.prototype._drawLines = function(context, toX, toY) {
 
 
-	var scalefactor = Math.log(Math.abs(this.axis.max - this.axis.min)) / 
-		Math.log(this.line_multiples);
+
+// Generate array of shades and drawing callables
+ReferenceLines.prototype._getDrawingArray = function(ref) {
 
 
-	var scales = [Math.floor(scalefactor) - 1, Math.floor(scalefactor)];
+	var scalefactor = Math.log(Math.abs(ref.axis.max - ref.axis.min)) / 
+		Math.log(ref.line_multiples);
+
+
 
 	// This should be implemented as a ratio of screen width/height
-	var scalelevels = 2;
-	
+	var scalelevels = 2;  // ref.axis.get_full_extent() / 250;
 
 
-	function moveTo(x, y) {
-		context.moveTo(toX(x), toY(y));
-	}
+	var a = [];
 
-	function lineTo(x, y) {
-		context.lineTo(toX(x), toY(y));
-	}
+	for(var i = 0; i < scalelevels; i++) {
 
+		var scale = Math.floor(scalefactor) - i;
+		var shade = ref.getShade(scale, scalefactor, scalelevels);
 
-	var labels = [];
-
-
-	for(var i = 0; i < scales.length; i++) {
-		var scale = scales[i];
-
-		var shade = Math.min(Math.max(scalefactor - scale, 0), scalelevels)
-			/ scalelevels;
-
-		var hex = Math.floor(shade * 255);
-
-		var color = 'rgb(' + hex + ', ' + hex + ', ' + hex + ')';
-		context.strokeStyle = color;
-
-
-		var interval = Math.pow(this.line_multiples, scale);
-
-
-		context.beginPath();
-
-		this._iterateIntervalOverAxis(interval, function(j) {
-
-			var startP = j,
-				endP = j,
-				startS = this.saxis.min,
-				endS = this.saxis.max;
-
-			
-			this.axis.orientationf(moveTo, startP, startS);
-			this.axis.orientationf(lineTo, endP, endS);
-
-		});
-
-		context.stroke();
+		a.push([shade, scale, function(context, toX, toY, scale) {
+			ref.drawLines(context, toX, toY, scale, scalefactor, scalelevels);
+		}]);
 
 	}
 
-};
 
-
-ReferenceLines.prototype._drawAxes = function(context, toX, toY) {
-
-
-	function moveTo(x, y) {
-		context.moveTo(toX(x), toY(y));
-	}
-
-	function lineTo(x, y) {
-		context.lineTo(toX(x), toY(y));
-	}
-
-	// Draw axis lines in blue
-
-	context.strokeStyle = '#0000FF';
-
-	var startP = 0,
-		endP = 0,
-		startS = this.saxis.min,
-		endS = this.saxis.max;
-
-	context.beginPath();
-
-	this.axis.orientationf(moveTo, startP, startS);
-	this.axis.orientationf(lineTo, endP, endS);
-
-	context.stroke();
-
-
-};
-
-ReferenceLines.prototype._drawLabel = function(context, toX, toY, offset, text) {
-
-
-	var centerX,
-		centerY;
-
-	if(this.axis.orientation) {
-		centerX = toX(offset);
-		centerY = 20;
-	} else {
-		centerX = 20;
-		centerY = toY(offset);
-	}
-
-
-	context.textAlign = 'center';
-	context.textBaseline = 'middle';
-	
-	// Returns undefined for some reason
-	var width = context.measureText(text).width;
-
-	// var width = 40;
-
-	context.fillStyle = '#F5F5F5';
-	context.fillRect(centerX - (width / 2), 
-		centerY - 7, 
-		width, 
-		14);
-
-	context.fillStyle = 'rgb(0, 0, 0)';
-	context.fillText(text, centerX, centerY);
-
-};
-
-ReferenceLines.prototype.drawLabels = function(context, toX, toY) {
-
-
-	var scalefactor = Math.log(Math.abs(this.axis.max - this.axis.min)) / 
-		Math.log(this.line_multiples);
-
-
-	var labels = [];
-	var interval = Math.pow(this.line_multiples, Math.floor(scalefactor));
-
-
-	this._iterateIntervalOverAxis(interval, function(j) {
-		labels.push([j, (Math.round(j * 1e10) / 1e10).toExponential()]);
-	});
-
-	for(var i = 0; i < labels.length; i++) {
-		var label = labels[i];
-
-		this._drawLabel(context, toX, toY, label[0], label[1]);
-	}
+	return a;
 
 };
 
 ReferenceLines.prototype.draw = function(context, toX, toY) {
 
-	this._drawLines(context, toX, toY);
-	this._drawAxes(context, toX, toY);
+
+	var xArr = this._getDrawingArray(this.xRef),
+		yArr = this._getDrawingArray(this.yRef);
+
+	var a = xArr.concat(yArr);
 
 
-	// This call is delegated to after draw calls of x/y reference axes to
-	// prevent overlapping.
+	// Sort by /descending/ shade. i.e. darker groups of lines
+	// are drawn later
+	a.sort(function(a, b) {
+		return b[0] - a[0];
+	});
 
-	// this.drawLabels(context, toX, toY);
+
+	for(var i = 0; i < a.length; i++) {
+		a[i][2](context, toX, toY, a[i][1]);
+	}
+
+
+
+	this.xRef.drawAxes(context, toX, toY);
+	this.yRef.drawAxes(context, toX, toY);
+
+	this.xRef.drawLabels(context, toX, toY);
+	this.yRef.drawLabels(context, toX, toY);
 
 };
 
