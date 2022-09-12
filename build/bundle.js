510ad6f9130602d23e03b8d5c464189c949c0d4c
Monty Thibault <montythibault@gmail.com>
2022-09-12 09:32:40 -0700

    Implemented special lines on scrollbars.

--- build/bundle.js
+++ build/bundle.js
@@ -760,6 +760,7 @@ function debounce(f, delay) {
 function Graph(canvas) {
 	
 	this.canvas = canvas;
+	
 
 	this.xAxis = new Axis(true, -5, 5, function() { return canvas.width; });
 	this.yAxis = new Axis(false, -5, 5, function() { return canvas.height; });
@@ -771,8 +772,9 @@ function Graph(canvas) {
 	this.reference.yRef.specialLabels.push([0, 'X', '#0000FF']);
 
 
-	this.xAxisRange = new RangeSlider(this.xAxis, this.yAxis);
-	this.yAxisRange = new RangeSlider(this.yAxis, this.xAxis);
+	this.xAxisRange = new RangeSlider(this.xAxis, this.yAxis, this.reference.xRef.specialLabels);
+	this.yAxisRange = new RangeSlider(this.yAxis, this.xAxis, this.reference.yRef.specialLabels);
+
 
 
 	this.mousecontrol = new MouseControl(this);
@@ -1374,10 +1376,12 @@ CornerDraggable.prototype.ondrag = function(x, y) {
 
 
 
-function RangeSlider(principal_axis, secondary_axis) {
+function RangeSlider(principal_axis, secondary_axis, specialLabels) {
 
 	this.axis = principal_axis;
 	this.saxis = secondary_axis;
+
+	this.specialLabels = specialLabels;
 
 	this.strokeColor;
 
@@ -1401,19 +1405,18 @@ function RangeSlider(principal_axis, secondary_axis) {
 	this._enabledCornerDrag = null;
 
 
+	// Bounding box
+
+	this.bb_height = 20;
+	this.bb_side_padding = 25;
+
+
 	this.onactiveend();
 
 }
 
 
-RangeSlider.prototype.draw = function(context, toX, toY) {
-
-
-	// Bounding box
-
-	var bb_height = 20;
-	var bb_side_padding = 25;
-
+RangeSlider.prototype._drawBB = function(context, toX, toY) {
 
 	if(this.axis.orientation) {
 
@@ -1421,18 +1424,18 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 			toS = toY;
 		
 		var startX = 0,
-		    startY = toY(this.saxis.max) - bb_height,
+		    startY = toY(this.saxis.max) - this.bb_height,
 		    width = toX(this.axis.max),
-		    height = bb_height;
+		    height = this.bb_height;
 
 	} else {
 
 		var toP = toY,
 			toS = toX;
 	
-		var startX = toX(this.saxis.max) - bb_height,
+		var startX = toX(this.saxis.max) - this.bb_height,
 		    startY = 0,
-		    width = bb_height,
+		    width = this.bb_height,
 		    height = toY(this.axis.max);
 
 	}
@@ -1443,10 +1446,68 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 
 	// These parameters provide padding from the canvas boundaries to the
 	// computed mins and maxes. i.e. min is inset 20px from the extreme left.
-	this.minP = bb_side_padding,
-	this.maxP = toP(this.axis.max) - bb_side_padding;
-	this.midS = toS(this.saxis.max) - (bb_height / 2);
+	this.minP = this.bb_side_padding,
+	this.maxP = toP(this.axis.max) - this.bb_side_padding;
+	this.midS = toS(this.saxis.max) - (this.bb_height / 2);
 
+};
+
+
+RangeSlider.prototype._drawSpecialLines = function(context, toX, toY) {
+
+	for(var i = 0; i < this.specialLabels.length; i++) {
+
+		var sl = this.specialLabels[i];
+
+
+		var diff = this.maxP - this.minP;
+
+		var p = this.minP + ((sl[0] - this.axis.minLimit) / 
+			(this.axis.maxLimit - this.axis.minLimit) * diff);
+
+
+		context.beginPath();
+
+		context.beginPath();
+		context.strokeStyle = sl[2];
+
+		if(sl.length === 4) {
+
+			context.setLineDash(sl[3]);
+
+		}
+
+
+		if(this.axis.orientation) {
+
+			var startX = p,
+				startY = toY(this.saxis.max) - this.bb_height,
+				endX = p,
+				endY = toY(this.saxis.max);
+
+		} else {
+
+			var startX = toX(this.saxis.max) - this.bb_height,
+				startY = p,
+				endX = toX(this.saxis.max),
+				endY = p;
+
+		}
+
+
+		context.moveTo(startX, startY);
+		context.lineTo(endX, endY);
+
+
+		context.stroke();
+		context.setLineDash([]);
+
+	}
+
+};
+
+
+RangeSlider.prototype._recomputeStartEndP = function() {
 
 
 	var diff = this.maxP - this.minP;
@@ -1456,12 +1517,7 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 
 	this.endP = this.minP + ((this.axis.max  - this.axis.minLimit) / 
 		(this.axis.maxLimit - this.axis.minLimit) * diff);
-	
 
-	this.updateCornerDrag();
-
-
-	// Draw corner circles
 
 	// Circles should not overlap
 	if(this.endP - this.startP < this.corner_drag_radius * 2) {
@@ -1472,6 +1528,11 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 
 	}
 
+};
+
+
+
+RangeSlider.prototype._drawCornerCircles = function(context, toX, toY) {
 
 	this.corner_max.p = this.endP;
 	this.corner_max.s = this.midS;
@@ -1482,8 +1543,10 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 	this.corner_min.draw(context);
 	this.corner_max.draw(context);
 
+};
 
-	// Draw connecting lines
+
+RangeSlider.prototype._drawConnectingLine = function(context, toX, toY) {
 
 	context.strokeStyle = this.strokeColor;
 	context.beginPath();
@@ -1502,6 +1565,21 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 	}
 
 	context.stroke();
+
+};
+
+
+RangeSlider.prototype.draw = function(context, toX, toY) {
+
+	this._drawBB(context, toX, toY);
+	this._drawSpecialLines(context, toX, toY);
+
+	this._recomputeStartEndP();
+	this.updateCornerDrag();
+
+	this._drawCornerCircles(context, toX, toY);
+	this._drawConnectingLine(context, toX, toY);
+
 
 };
 
--- src/rangeslider.js
+++ src/rangeslider.js
@@ -125,10 +125,12 @@ CornerDraggable.prototype.ondrag = function(x, y) {
 
 
 
-function RangeSlider(principal_axis, secondary_axis) {
+function RangeSlider(principal_axis, secondary_axis, specialLabels) {
 
 	this.axis = principal_axis;
 	this.saxis = secondary_axis;
+
+	this.specialLabels = specialLabels;
 
 	this.strokeColor;
 
@@ -152,19 +154,18 @@ function RangeSlider(principal_axis, secondary_axis) {
 	this._enabledCornerDrag = null;
 
 
+	// Bounding box
+
+	this.bb_height = 20;
+	this.bb_side_padding = 25;
+
+
 	this.onactiveend();
 
 }
 
 
-RangeSlider.prototype.draw = function(context, toX, toY) {
-
-
-	// Bounding box
-
-	var bb_height = 20;
-	var bb_side_padding = 25;
-
+RangeSlider.prototype._drawBB = function(context, toX, toY) {
 
 	if(this.axis.orientation) {
 
@@ -172,18 +173,18 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 			toS = toY;
 		
 		var startX = 0,
-		    startY = toY(this.saxis.max) - bb_height,
+		    startY = toY(this.saxis.max) - this.bb_height,
 		    width = toX(this.axis.max),
-		    height = bb_height;
+		    height = this.bb_height;
 
 	} else {
 
 		var toP = toY,
 			toS = toX;
 	
-		var startX = toX(this.saxis.max) - bb_height,
+		var startX = toX(this.saxis.max) - this.bb_height,
 		    startY = 0,
-		    width = bb_height,
+		    width = this.bb_height,
 		    height = toY(this.axis.max);
 
 	}
@@ -194,10 +195,68 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 
 	// These parameters provide padding from the canvas boundaries to the
 	// computed mins and maxes. i.e. min is inset 20px from the extreme left.
-	this.minP = bb_side_padding,
-	this.maxP = toP(this.axis.max) - bb_side_padding;
-	this.midS = toS(this.saxis.max) - (bb_height / 2);
+	this.minP = this.bb_side_padding,
+	this.maxP = toP(this.axis.max) - this.bb_side_padding;
+	this.midS = toS(this.saxis.max) - (this.bb_height / 2);
 
+};
+
+
+RangeSlider.prototype._drawSpecialLines = function(context, toX, toY) {
+
+	for(var i = 0; i < this.specialLabels.length; i++) {
+
+		var sl = this.specialLabels[i];
+
+
+		var diff = this.maxP - this.minP;
+
+		var p = this.minP + ((sl[0] - this.axis.minLimit) / 
+			(this.axis.maxLimit - this.axis.minLimit) * diff);
+
+
+		context.beginPath();
+
+		context.beginPath();
+		context.strokeStyle = sl[2];
+
+		if(sl.length === 4) {
+
+			context.setLineDash(sl[3]);
+
+		}
+
+
+		if(this.axis.orientation) {
+
+			var startX = p,
+				startY = toY(this.saxis.max) - this.bb_height,
+				endX = p,
+				endY = toY(this.saxis.max);
+
+		} else {
+
+			var startX = toX(this.saxis.max) - this.bb_height,
+				startY = p,
+				endX = toX(this.saxis.max),
+				endY = p;
+
+		}
+
+
+		context.moveTo(startX, startY);
+		context.lineTo(endX, endY);
+
+
+		context.stroke();
+		context.setLineDash([]);
+
+	}
+
+};
+
+
+RangeSlider.prototype._recomputeStartEndP = function() {
 
 
 	var diff = this.maxP - this.minP;
@@ -207,12 +266,7 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 
 	this.endP = this.minP + ((this.axis.max  - this.axis.minLimit) / 
 		(this.axis.maxLimit - this.axis.minLimit) * diff);
-	
 
-	this.updateCornerDrag();
-
-
-	// Draw corner circles
 
 	// Circles should not overlap
 	if(this.endP - this.startP < this.corner_drag_radius * 2) {
@@ -223,6 +277,11 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 
 	}
 
+};
+
+
+
+RangeSlider.prototype._drawCornerCircles = function(context, toX, toY) {
 
 	this.corner_max.p = this.endP;
 	this.corner_max.s = this.midS;
@@ -233,8 +292,10 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 	this.corner_min.draw(context);
 	this.corner_max.draw(context);
 
+};
 
-	// Draw connecting lines
+
+RangeSlider.prototype._drawConnectingLine = function(context, toX, toY) {
 
 	context.strokeStyle = this.strokeColor;
 	context.beginPath();
@@ -253,6 +314,21 @@ RangeSlider.prototype.draw = function(context, toX, toY) {
 	}
 
 	context.stroke();
+
+};
+
+
+RangeSlider.prototype.draw = function(context, toX, toY) {
+
+	this._drawBB(context, toX, toY);
+	this._drawSpecialLines(context, toX, toY);
+
+	this._recomputeStartEndP();
+	this.updateCornerDrag();
+
+	this._drawCornerCircles(context, toX, toY);
+	this._drawConnectingLine(context, toX, toY);
+
 
 };
 
